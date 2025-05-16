b5290951753daa5b8864c6be4ac6187058b3d9ae
Zablon Charles <zablon.charles@gmail.com>
2025-05-16 14:19:40 -0400

    Initial Commit

---
+++ CastAppPro/CastAppProApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastAppProApp.swift
+//  CastAppPro
+//
+//  Created by Zablon Charles on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CastAppProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
